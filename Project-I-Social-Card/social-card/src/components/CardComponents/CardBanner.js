df166f34a159f53ff86273cadcf99704834908d9
Ariunbold Oyungerel <abc@example.com>
2019-05-29 19:19:37 -0400

    finished JS part

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+      <div className="banner-logo"
+            href= 'https://reactjs.org'>
+        <img 
+        className="banner-img" 
+        src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
+        alt = "react Logo"
+        />
+      </div>
+    );
+  };
+  
+  export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+// import ImageThumbnail from './ImageThumbnail'
+import HeaderContent from "./HeaderContent";
+
+const HeaderTitle = () => {
+  return (
+    <div className="header-title">
+      <h1>Lambda School</h1>
+      <p className="fineprint">@LambdaSchool 26 Jan</p>
+      <HeaderContent/>
+    </div>
+  );
+};
+
+export default HeaderTitle;
